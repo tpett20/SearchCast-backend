477ee05f1f56a8606cc440d381b6c71c0e7c6d19
tpett20 <thomaspettersen20@gmail.com>
2023-05-09 11:16:46 -0400

    stub searches index and show route, both hit successfully w Postman

--- routes/searches-router.js
+++ routes/searches-router.js
@@ -4,9 +4,20 @@ const router = express.Router()
 
 // ROUTES
 
+// Searches Index Route
+router.get('/', async (req, res) => {
+    res.status(200).json({message: 'Hit Searches Index Route'})
+})
+
 // Searches Create Route
 router.post('/', async (req, res) => {
+    console.log(req.body)
     res.status(201).json({message: 'Hit Searches Create Route'})
+})
+
+// Searches Show Route
+router.get('/:id', async (req, res) => {
+    res.status(200).json({message: 'Hit Searches Show Route: ' + req.params.id})
 })
 
 module.exports = router
